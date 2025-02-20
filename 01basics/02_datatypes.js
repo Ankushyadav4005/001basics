2f98643b3d65121507177d8dd6e824df7bdfaba3
Ankushyadav4005 <144465786+Ankushyadav4005@users.noreply.github.com>
2025-02-20 10:21:19 +0000

    datatype

---
+++ 01basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict"  // treat all JS code newer version  
+
+// alert(3+3)    // we are using nodejs , not browser
+
+
+let name="Ankush"  // it is string datatype
+let age=22  // it is numeric datatype
+let isLoggedIn=false // it is boolean type data type 
+
+
+//number => range 2->53
+//bigint =>  to store the big values
+//strings  => ""
+//boolean => true /false;
+// null => standlone values
+// undefined
+// symbol => to find uniqueness
+
+
+// object
+
+
+console.log(typeof "ankush" );
+console.log(typeof age);
+
+console.log(typeof null);   //  =>  output=> object
+console.log(typeof undefined);  // => output =>undefined
