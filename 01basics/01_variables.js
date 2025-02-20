e311d5771cae1992f85598cea7c8bfe417dfbca3
Ankushyadav4005 <144465786+Ankushyadav4005@users.noreply.github.com>
2025-02-20 10:16:37 +0000

    datatypes

---
+++ 01basics/01_variables.js
@@ -0,0 +1,19 @@
+const account_Id=144553 
+let account_Email="ankushyadav4005@gamil.com"
+var account_password="5555"
+account_city="jaipur"
+
+//account_Id="00000"   --> this is not possible reassining the val beacue we use the cont keyword
+//console.log(account_Id);
+
+
+account_Email="elu@1234"
+account_password="0000"
+account_city="banguluru"
+let account_state;
+console.table([account_Email,account_Id,account_password,account_city,account_state]);
+/*
+please never use the var keyword 
+beacuse of issue of block scope or functinal scope 
+*/
+
