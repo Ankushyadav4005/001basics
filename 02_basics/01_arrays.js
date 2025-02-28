acd603189d79a4412dcae86f7c47e4cad8da4411
Ankushyadav4005 <144465786+Ankushyadav4005@users.noreply.github.com>
2025-02-28 21:17:52 +0000

    array

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,22 @@
+
+// ***********Array****************
+const myArr=[1,2,3,4,5,6,7]
+const arr1=["saktiman", "naagraj","ankush"]
+const arr2= new Array(1,2,3,4,5)
+console.log(myArr[0]);
+console.log(myArr.length);
+myArr.push(8)
+console.log(myArr.length);
+myArr.pop()
+console.log(myArr.length);
+console.log(myArr)
+myArr.unshift(0)
+myArr.unshift(0)
+console.log(myArr);
+console.log(myArr.includes(9))
+console.log(myArr.indexOf(9))
+
+const newArr= myArr.join()
+console.log(myArr)
+console.log(newArr)
+console.log(typeof newArr)
